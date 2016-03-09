fix wrong shape function

stokes/functions/finite_elements.py
@@ -22,7 +22,7 @@ def P1ShapeFunctions(dim):
         ))
     elif dim == 2:
         return lambda X: np.array([
-            1.0 - X[..., 0],
+            1.0 - X[..., 0] - X[..., 1],
             X[..., 0],
             X[..., 1]
         ])